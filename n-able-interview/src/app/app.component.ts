d019377b3eac92801294672fc0d9e44e9d98f0c3
vzlatin <victorzlatin0@gmail.com>
2025-04-02 00:15:53 +0300

    fix: Move the code from app component to home component

--- n-able-interview/src/app/app.component.ts
+++ n-able-interview/src/app/app.component.ts
@@ -1,17 +1,12 @@
 import { Component, inject } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import { GithubService } from "./services/github.service";
-import { AsyncPipe } from "@angular/common";
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet, AsyncPipe],
+  imports: [RouterOutlet],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
 export class AppComponent {
   title = "n-able-interview";
-  githubService = inject(GithubService);
-  users$ = this.githubService.users$;
-  usersError$ = this.githubService.usersError$;
 }
--- n-able-interview/src/app/components/home/home.component.ts
+++ n-able-interview/src/app/components/home/home.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from "@angular/core";
+import { GithubService } from "../../services/github.service";
+import { AsyncPipe } from "@angular/common";
 
 @Component({
-  selector: 'app-home',
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  selector: "app-home",
+  imports: [AsyncPipe],
+  templateUrl: "./home.component.html",
+  styleUrl: "./home.component.css",
 })
 export class HomeComponent {
-
+  githubService = inject(GithubService);
+  users$ = this.githubService.users$;
+  usersError$ = this.githubService.usersError$;
 }
